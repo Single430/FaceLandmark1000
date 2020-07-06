f2a0f60e9e3251721560373aa2a6d80cf29e778e
super.single430 <Single430@users.noreply.github.com>
2020-07-06 11:29:40 +0800

    Update demo.py
    增加实时视频流显示及结果输出mp4

--- demo.py
+++ demo.py
@@ -39,6 +39,58 @@ def image_run():
         landmarks, states = face_landmark_handle.run(image, detection)
         face_landmark_handle.show_result(image, landmarks)
 
+        
+def video_capture_run():
+    face_detector_handle = FaceDetector()
+    face_landmark_handle = FaceLandmark()
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('out.mp4', fourcc, 10, (640, 480))
+    while True:
+        t1 = cv2.getTickCount()
+        ret, frame = cap.read()
+        if ret == True:
+            detections, landmarks = face_detector_handle.run(frame)
+            t2 = cv2.getTickCount()
+            t = (t2 - t1) / cv2.getTickFrequency()
+            fps = 1.0 / t
+            for i in range(detections.shape[0]):
+                bbox = detections[i, :4]
+                score = detections[i, 4]
+                corpbbox = [int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])]
+
+                # 画人脸框
+                cv2.rectangle(frame, (corpbbox[0], corpbbox[1]),
+                              (corpbbox[2], corpbbox[3]), (255, 0, 0), 1)  # -1, 4)
+                # 画置信度
+                cv2.putText(frame, '{:.2f}'.format(score),
+                            (corpbbox[0], corpbbox[1] - 2),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.5, (0, 0, 255), 2)
+                # 画fps值
+            cv2.putText(frame, '{:.4f}'.format(t) + " " + '{:.3f}'.format(fps), (10, 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
+            # 画关键点
+            if len(detections) == 0:
+                return
+
+            for detection in detections:
+                landmarks, states = face_landmark_handle.run(frame, detection)
+                for i in range(landmarks.shape[0]):
+                    for j in range(len(landmarks[i]) // 2):
+                        cv2.circle(frame, (int(landmarks[i][2 * j]), int(int(landmarks[i][2 * j + 1]))), 2, (0, 0, 255))
+                # face_landmark_handle.show_result(frame, landmarks)
+            a = out.write(frame)
+            cv2.imshow("result", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
-    image_run()
+    # image_run()
+    video_capture_run()
